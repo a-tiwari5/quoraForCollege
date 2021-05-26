3b60072bf58cb1cca9867e9a6975d61836e95d20
Adarsh Tiwari <adarshtiwari0395@gmail.com>
2021-05-26 11:02:33 +0530

    added a landing page to QuoraForCollege

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.send('WHAT THE FUCK IS DIS?')
+    res.render('home')
 })
 
 
